346b0f3bdde060e3e7fb66809e74233fe462b635
ranya <ranya.zharylkhassyn@fizmat.kz>
2023-02-02 15:04:31 +0600

    lsn_4_jwt

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const config = require('config')
-const authRouter = require("./routes/auth_route")
+const express = require("express")
+const mongoose = require("mongoose")
+const config = require("config")
+const authRouter = require("./routes/auth.routes")
 const app = express()
 const PORT = config.get('serverPort')
 
@@ -9,16 +9,18 @@ app.use(express.json())
 app.use("/api/auth", authRouter)
 
 
-
 const start = async () => {
     try {
-        await mongoose.connect(config.get('dbUrl'))
+        await mongoose.connect(config.get("dbUrl"), {
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
 
         app.listen(PORT, () => {
-           console.log('Server started on port:', PORT)
+            console.log('Server started on port ', PORT)
         })
     } catch (e) {
-
+        console.log(e)
     }
 }
 
